4f4a1058c002c93fe6be7f11157b01f0f5e53f94
Ivan-Roger <ivan.roger@iut2.upmf-grenoble.fr>
2015-11-06 17:16:14 +0100

    Up

---
+++ code/view/WIP.view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <header>
+    <meta charset="utf-8"/>
+    <title>Daelium | Work in Progress</title>
+  </header>
+  <body>
+    <h1>Daelium | Work in Progress</h1>
+  </body>
+</html>
