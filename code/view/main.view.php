677e9b257462f869e22a96cdc3e86d54d07d3329
BARBIER Etienne <barbieti@hostname.iut2.upmf-grenoble.fr>
2015-11-06 17:26:30 +0100

    view

---
+++ code/view/main.view.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  <title>Daelium</title>
+</head>
+<body>
+  <header class="page-header">
+    <div id="top">
+      <img src="http://4vector.com/i/free-vector-nycs-bull-trans-d-clip-art_109778_Nycs_Bull_Trans_D_clip_art_hight.png" alt="LogoSite" style="height:100px;">
+      <h1>Daelium</h1>
+      <p>Le site des echanges musicaux</p>
+      <!-- Logo du site , tritre description -->
+    </div>
+    <nav class="col-lg-12">
+      <!-- Menu des differentes section du site  plus module de recherche-->
+      <ul class="nav nav-tabs col-lg-6">
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Profil</a></li>
+        <li><a href="#">Documents</a></li>
+        <li><a href="#">Agenda</a></li>
+        <li><a href="#">Messagerie</a></li>
+        <li><a href="#">Annuaire</a></li>
+      </ul>
+      <form class="form-horizontal col-lg-4" method="get" action="#">
+        <div class="col-lg-6"><input class="form-control" type="text" name="recherche" id="recherche" placeholder="Recherche" /></div>
+        <input class="btn btn-primary" type="submit" value="Rechercher">
+      </form>
+    <div id="usercontent" class="col-lg-2">
+      <!-- Menu de l'uilisateur-->
+
+
+      <ul id="preference" class="col-lg-6">
+        <li><a href="#">#</a></li>
+        <li><a href="#">#</a></li>
+      </ul>
+      <ul id="profil" class="col-lg-6">
+        <li><a href="#">#</a></li>
+        <li><a href="#">#</a></li>
+      </ul>
+    </div>
+  </nav>
+  </header>
+  <section class="col-sm-10">
+    <!-- Alertes et information pour l'utilisateur -->
+    <article>
+      <img src="http://www.ac-grenoble.fr/ien.vienne1-2/spip/IMG/bmp_Image004.bmp" alt="Mountain View" style="height:100px;">
+      <p>Charle DURAND à rendu libre les inscription pour son evenement "Festival des eaux de la pluie". Vous pouvez maintenent rentre en contact avec lui afin d'y Inscrire un Artiste/Groupe .</p>
+    </article>
+    <article>
+      <img src="http://www.ac-grenoble.fr/ien.vienne1-2/spip/IMG/bmp_Image004.bmp" alt="Mountain View" style="height:100px;">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+    </article>
+    <article>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+    </article>
+    <article>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+    </article>
+  </section>
+  <section class="col-sm-2">
+    <!-- Sugestion pour l'utilisateur (evt & art pour booker) (art pour orga )-->
+    <article>
+      <img src="http://www.ac-grenoble.fr/ien.vienne1-2/spip/IMG/bmp_Image004.bmp" alt="Mountain View" style="height:100px;">
+      <p>L'evenement Bringue a la mairie de Valence est suceptible d'interesser le Groupe/Artiste "Les Bleu" que vous gerez.</p>
+    </article>
+    <article>
+      <p>L'evenement Bringue a la mairie de Valence est suceptible d'interesser le Groupe/Artiste "Les Bleu" que vous gerez.</p>
+    </article>
+  </section>
+  <div id="footer">
+    <h2>Daelium</h2>
+    <p>IUT2 UPMF, Place Doyen Gosse, 38000 Grenoble, Rhone-Alpes</p>
+  </div>
+</body>
+</html>
