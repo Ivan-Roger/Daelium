7afceed6844246a28e036ed15e77bb8dfacf5d65
Ivan-Roger <ivan.bob.emile@gmail.com>
2015-11-16 19:36:24 +0100

    Modif page accueil (nolog) + ajout icons

--- code/view/nolog.view.php
+++ code/view/nolog.view.php
@@ -7,17 +7,6 @@
     <title>Dælium</title>
   </head>
   <body>
-    <nav class="container-fluid nav navbar-default">
-      <div class="navbar-header">
-        <img src="../data/img/D.png" alt="LogoSite" style="height:50px;">
-      </div>
-      <!-- Menu des differentes section du site  plus module de recherche-->
-      <form class="navbar-form navbar-right" method="GET" action="#">
-        <!-- Menu de l'uilisateur-->
-          <a href="signin.view.php" type="button" class="btn btn-primary">Se connecter</a>
-          <a href="signup.view.php" type="button" class="btn btn-warning">Je m'inscrit maintenent !</a>
-      </form>
-    </nav>
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
       <!-- Indicators -->
       <ol class="carousel-indicators">
@@ -27,17 +16,19 @@
 
       <!-- Wrapper for slides -->
       <div class="carousel-inner" role="listbox">
-        <div class="item active">
+        <div class="slide-full item active">
+          <!--<img src="../data/img/fotolia_musique.jpg" style="position: relative; bottom: 500px;"/>-->
           <div class="carousel-caption">
-            <h1>Slide 1</h1>
-            <p>...</p>
+            <h1>Bienvenue sur <b>Dælium</b></h1>
+            <p>Bienvenue sur le site de gestion musical.</p>
           </div>
         </div>
-        <div class="item">
+        <div class="slide-full item">
+          <!--<img src="../data/img/fotolia_musique.jpg" style="position: relative; bottom: 500px;"/>-->
           <div class="carousel-caption">
             <h1>Another example headline.</h1>
             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+            <p><a class="btn btn-lg btn-primary" href="#navbar" role="button">Learn more</a></p>
           </div>
         </div>
       </div>
@@ -53,27 +44,64 @@
       </a>
     </div>
 
+    <div class="btn-down container-fluid">
+      <a href="#navbar" class="">Lire ...</a>
+    </div>
+
+    <nav id="navbar" class="container-fluid nav navbar-default">
+      <div class="navbar-header">
+        <img src="../data/img/D.png" alt="LogoSite" style="height:50px;"/>
+      </div>
+      <!-- Menu des differentes section du site  plus module de recherche-->
+      <form class="navbar-form navbar-right" method="GET" action="#">
+        <!-- Menu de l'uilisateur-->
+          <a href="signin.view.php" type="button" class="btn btn-primary">Se connecter</a>
+          <a href="signup.view.php" type="button" class="btn btn-warning">Je m'inscrit maintenent !</a>
+      </form>
+    </nav>
+
     <div class="container marketing">
       <div class="row">
-              <div class="col-lg-4">
-                <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                <h2>Heading</h2>
-                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-              </div><!-- /.col-lg-4 -->
-              <div class="col-lg-4">
-                <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                <h2>Heading</h2>
-                <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-                <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-              </div><!-- /.col-lg-4 -->
-              <div class="col-lg-4">
-                <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                <h2>Heading</h2>
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-              </div><!-- /.col-lg-4 -->
-            </div><!-- /.row -->
+        <div class="col-lg-4">
+          <div class="img-circle"><span class="icon-img icon-gift"></span></div>
+          <h2>Gratuit</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <div class="img-circle"><span class="icon-img icon-tools2"></span></div>
+          <h2>Innovant</h2>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <div class="img-circle"><span class="icon-img icon-adjustments"></span></div>
+          <h2>Adaptable</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+          <caption class="on-pic"><span class="glyphicon glyphicon-gift"></span></caption>
+          <h2>Gratuit</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+          <h2>Heading</h2>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
     </div>
 
     <?php include("../view/include/footer.view.php"); ?>
