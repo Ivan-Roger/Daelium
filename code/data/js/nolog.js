f893cd22a69643d0fa773c5136c3e812e25a9209
Ivan-Roger <ivan.bob.emile@gmail.com>
2015-11-17 22:06:50 +0100

    Modif de nolog, encore (barre fixée quand on descend)

---
+++ code/data/js/nolog.js
@@ -0,0 +1,13 @@
+function attachNavbar() {
+  if (window.pageYOffset >= $("#navbarOrg").offset().top) {
+    console.log("Navbar attached");
+    $("#navbar").css("position","fixed");
+    $("#navbar").css("top","0px");
+    $("#navbar").css("left","0px");
+    $("#navbarOrg").css("margin-bottom","50px");
+  } else {
+    console.log("Navbar distached");
+    $("#navbarOrg").css("margin-bottom","0px");
+    $("#navbar").css("position","relative");
+  }
+}
--- code/view/nolog.view.php
+++ code/view/nolog.view.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="../data/css/nolog.css"/>
     <title>Dælium</title>
   </head>
-  <body>
+  <body onscroll="attachNavbar()">
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
       <!-- Indicators -->
       <ol class="carousel-indicators">
@@ -48,6 +48,7 @@
       <a href="#navbar" class="">Lire ...</a>
     </div>
 
+    <div id="navbarOrg">.</div>
     <nav id="navbar" class="container-fluid nav navbar-default">
       <div class="navbar-header">
         <img src="../data/img/D.png" alt="LogoSite" style="height:50px;"/>
@@ -56,25 +57,30 @@
       <form class="navbar-form navbar-right" method="GET" action="#">
         <!-- Menu de l'uilisateur-->
           <a href="signin.view.php" type="button" class="btn btn-primary">Se connecter</a>
-          <a href="signup.view.php" type="button" class="btn btn-warning">Je m'inscrit maintenent !</a>
+          <a href="signup.view.php" type="button" class="btn btn-warning">Je m'inscrit maintenant !</a>
       </form>
     </nav>
 
     <div class="container marketing">
       <div class="row">
-        <div class="col-lg-4">
+        <blockquote>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rhoncus vulputate tellus, eget ornare nibh eleifend sed. Etiam efficitur nisi sit amet laoreet efficitur. Maecenas sit amet scelerisque erat, sit amet facilisis erat. Praesent facilisis, elit sed posuere faucibus, risus eros pulvinar ex, nec ornare sapien enim id nulla. Aliquam at lacinia quam. Fusce laoreet ultricies enim, eu elementum nunc venenatis non. Aenean molestie enim nec dui consequat interdum. Morbi ipsum nisl, bibendum non eros in, gravida hendrerit purus. Donec orci risus, consequat at nisl eu, maximus ullamcorper erat. Nam in libero diam. Quisque et molestie sem, non tempor turpis. Aliquam erat volutpat.
+        </blockquote>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 text-center">
           <div class="img-circle"><span class="icon-img icon-gift"></span></div>
           <h2>Gratuit</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
+        <div class="col-lg-4 text-center">
           <div class="img-circle"><span class="icon-img icon-tools2"></span></div>
           <h2>Innovant</h2>
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
+        <div class="col-lg-4 text-center">
           <div class="img-circle"><span class="icon-img icon-adjustments"></span></div>
           <h2>Adaptable</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
@@ -82,24 +88,18 @@
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
       <div class="row">
-        <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <caption class="on-pic"><span class="glyphicon glyphicon-gift"></span></caption>
-          <h2>Gratuit</h2>
+        <div class="col-lg-8">
+          <h2>Rejoindre Daelium</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rhoncus vulputate tellus, eget ornare nibh eleifend sed. Etiam efficitur nisi sit amet laoreet efficitur. Maecenas sit amet scelerisque erat, sit amet facilisis erat. Praesent facilisis, elit sed posuere faucibus, risus eros pulvinar ex, nec ornare sapien enim id nulla.</p>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p class="text-right"><a class="btn btn-default btn-lg" href="#" role="button">Inscription &raquo;</a><br/><a style="font-size: 10px;" href="#" role="button">Connexion &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
+          <h2>Contact</h2>
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rhoncus vulputate tellus, eget ornare nibh eleifend sed. Etiam efficitur nisi sit amet laoreet efficitur. Maecenas sit amet scelerisque erat, sit amet facilisis erat. Praesent facilisis, elit sed posuere faucibus, risus eros pulvinar ex, nec ornare sapien enim id nulla.</p>
+          <p><a class="btn btn-default" href="#" role="button">Nous contacter &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
     </div>
@@ -107,5 +107,6 @@
     <?php include("../view/include/footer.view.php"); ?>
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="../data/js/bootstrap.min.js"></script>
+    <script src="../data/js/nolog.js"></script>
   </body>
 </html>
