3a7294e4eef909a0acc140e373edf763424529d2
EtienneBarbier <etienne.jeux@gmail.com>
2015-11-18 21:03:24 +0100

    Debut Artistes

--- code/view/artistes.view.php
+++ code/view/artistes.view.php
@@ -1,17 +1,34 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="../data/css/bootstrap.css">
-    <title>Dælium - Mes artistes</title>
-  </head>
-  <body>
-    <?php include("../view/include/header.view.php"); ?>
-    <section class="col-lg-12">
-      <p>Mes artistes ...</p>
-    </section>
-    <?php include("../view/include/footer.view.php"); ?>
-    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="../data/js/bootstrap.min.js"></script>
-  </body>
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="../data/css/bootstrap.css">
+  <title>Dælium - Mes artistes</title>
+</head>
+<body>
+  <?php include("../view/include/header.view.php"); ?>
+  <section class="col-lg-offset-1 col-lg-10">
+    <div class="row">
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="../data/users/users1/coverartistes/doogg.jpg" alt="...">
+        </a>
+        </div>
+        <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="../data/users/users1/coverartistes/mgims.jpg" alt="...">
+        </a>
+        </div>
+        <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="../data/users/users1/coverartistes/toy.jpg" alt="...">
+        </a>
+        </div>
+
+    </div>
+  </section>
+  <?php include("../view/include/footer.view.php"); ?>
+  <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script src="../data/js/bootstrap.min.js"></script>
+</body>
 </html>
