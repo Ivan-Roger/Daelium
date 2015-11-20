a99526a101571ac42bb374dc4d603da3e78494c9
Katravan <florian.inard@live.fr>
2015-11-20 16:18:56 +0100

    Update profile.view.php

--- code/view/profile.view.php
+++ code/view/profile.view.php
@@ -8,8 +8,97 @@
   <body>
     <?php include("../view/include/header.view.php"); ?>
     <section class="col-lg-12">
-      <p>Profil ...</p>
-      <h1>A FAIRE !!!</h1>
+
+      <h1> Informations personnelles : </h1>
+      <div class="col-sm-2">
+        <img src="../data/img/Avatar.jpg" alt="Image de Profil">
+      </div>
+
+      <form class="form-horizontal">
+
+        <div class="form-group">
+          <div class="row">
+          <label for="Nom" class="col-sm-1 control-label">Nom</label>
+            <div class="col-sm-2">
+              <input type="text" class="form-control" id="Nom" placeholder="Nom">
+            </div>
+            <div class="col-sm-2">
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Public
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="row">
+          <label for="Prenom" class="col-sm-1 control-label">Prénom</label>
+            <div class="col-sm-2">
+              <input type="text" class="form-control" id="Prenom" placeholder="Prénom">
+            </div>
+            <div class="col-sm-2">
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Public
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="row">
+          <label for="EmailPerso" class="col-sm-1 control-label">Email du compte</label>
+            <div class="col-sm-2">
+              <input type="email" class="form-control" id="EmailPerso" placeholder="Ex: prenom.nom@domaine.com">
+            </div>
+            <div class="col-sm-2">
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Public
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="row">
+          <label for="EmailPro" class="col-sm-1 control-label">Email de contact</label>
+            <div class="col-sm-2">
+              <input type="email" class="form-control" id="EmailPro" placeholder="Ex: prenom.nom@domaine.com">
+            </div>
+            <div class="col-sm-2">
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Public
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="row">
+          <label for="TelPro" class="col-sm-1 control-label">Numero de télephone professionnel</label>
+            <div class="col-sm-2">
+              <input type="tel" class="form-control" id="TelPro" placeholder="Format: XX XX XX XX XX ou XXXXXXXX">
+            </div>
+            <div class="col-sm-2">
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Public
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+</form>
+
+
     </section>
     <?php include("../view/include/footer.view.php"); ?>
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
