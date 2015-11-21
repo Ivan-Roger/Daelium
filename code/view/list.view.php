c72e3aac7654693121f3a0f8f445028d1168acb8
Ivan-Roger <ivan.bob.emile@gmail.com>
2015-11-21 19:19:17 +0100

    Modifs Messagerie et Annuaire

--- code/view/list.view.php
+++ code/view/list.view.php
@@ -10,103 +10,186 @@
 <body>
   <?php include("../view/include/header.view.php"); ?>
   <section>
-  <article id="boutons" class="col-lg-offset-1 col-lg-10 ">
-    <button class="btn btn-primary pull-right" type="submit">Ajouter un Contact</button>
-  </article>
-  <article class="col-lg-offset-1 col-lg-10 ">
-    <div class="panel panel-warning">
-      <div class="panel-heading"><strong>Contacts Favoris</strong></div>
-      <div class="panel-body table-responsive">
-        <table id="list" class="table table-bordered col-lg-12">
-          <colgroup>
-            <col/>
-            <col/>
-            <col/>
-            <col/>
-            <col/>
-            <col class="col-lg-2"/>
-          </colgroup>
-          <thead>
-            <tr><th>Nom / Prenom</th><th>Mail</th><th>Telephone</th><th>Description</th><th>Notes</th><th>Actions</th></tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>Jean Charle</th>
-              <td>jean.c@gmail.com</td>
-              <td>0600000000</td>
-              <td>Organisateur du festival les rouge</td>
-              <td>Serieux</td>
-              <td>
-                <div class="btn-group" role="group" aria-label="...">
-                  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-star-empty"></button>
-                  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-envelope"></button>
-                  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-user"></button>
-                  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></button>
-                  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-trash"></button>
-                </div>
-              </td>
-              <tr><th>Alphonse</th> <td>alphonse@gmail.com</td>  <td>0600000000</td>  <td>Booker des TheWorld</td> <td>De confiance</td><td>
-                <div class="btn-group" role="group" aria-label="...">
-                  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-star-empty"></button>
-                  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-envelope"></button>
-                  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-user"></button>
-                  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></button>
-                  <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-trash"></button>
-                </div></td></tr>
-            </tobdy>
-          </table>
+    <article class="col-lg-offset-1 col-lg-10">
+      <div class="navbar navbar-form navbar-right">
+        <div class="input-group">
+          <input type="text" class="form-control"/>
+          <div class="input-group-btn">
+            <a href="#" class="btn btn-default">Rechercher</a>
+          </div>
         </div>
+        <button class="btn btn-primary" type="submit">Ajouter un Contact</button>
       </div>
     </article>
-  <article class="col-lg-offset-1 col-lg-10 ">
-    <div class="panel panel-default">
-      <div class="panel-heading"><strong>Tout les contacts</strong></div>
-      <div class="panel-body table-responsive">
-        <table id="list" class="table table-bordered col-lg-12">
-          <colgroup>
-            <col/>
-            <col/>
-            <col/>
-            <col/>
-            <col/>
-            <col class="col-lg-1"/>
-          </colgroup>
-          <thead>
-            <tr><th>Nom / Prenom</th><th>Mail</th><th>Telephone</th><th>Description</th><th>Notes</th><th>Actions</th></tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>Jean Charle</th>
-              <td>jean.c@gmail.com</td>
-              <td>0600000000</td>
-              <td>Organisateur du festival les rouge</td>
-              <td>Serieux</td>
-              <td><table class="table table-bordered text-center">
-                <tr>
-                  <td><span class="glyphicon glyphicon-star"></span></td>
-                  <td><span class="glyphicon glyphicon-envelope"></span></td>
-                  <td><span class="glyphicon glyphicon-user"></span></td>
-                  <td><span class="glyphicon glyphicon-pencil"></span></td>
-                  <td><span class="glyphicon glyphicon-trash"></span></td>
-                </tr>
-              </table></td>
-              <tr><th>Alphonse</th> <td>alphonse@gmail.com</td>  <td>0600000000</td>  <td>Booker des TheWorld</td> <td>De confiance</td><td><table class="table table-bordered text-center">
-                <tr>
-                  <td><span class="glyphicon glyphicon-star"></span></td>
-                  <td><span class="glyphicon glyphicon-envelope"></span></td>
-                  <td><span class="glyphicon glyphicon-user"></span></td>
-                  <td><span class="glyphicon glyphicon-pencil"></span></td>
-                  <td><span class="glyphicon glyphicon-trash"></span></td>
-                </tr>
-              </table></td></tr>
-            </tobdy>
-          </table>
+    <article class="col-lg-offset-1 col-lg-10">
+      <div class="panel panel-warning">
+        <div class="panel-heading"><strong>Contacts Favoris</strong></div>
+        <div class="panel-body table-responsive">
+          <table id="list" class="table table-bordered col-lg-12">
+            <colgroup>
+              <col/>
+              <col/>
+              <col/>
+              <col/>
+              <col/>
+              <col class="col-lg-2"/>
+            </colgroup>
+            <thead>
+              <tr><th>Nom / Prenom</th><th>Mail</th><th>Telephone</th><th>Description</th><th>Notes</th><th>Actions</th></tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Jean Charle</th>
+                <td>jean.c@gmail.com</td>
+                <td>0600000000</td>
+                <td>Organisateur du festival les rouge</td>
+                <td>Serieux</td>
+                <td>
+                  <div class="btn-group" role="group" aria-label="...">
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-star-empty no-margin"></button>
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-envelope no-margin"></button>
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-user no-margin"></button>
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil no-margin"></button>
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-trash no-margin"></button>
+                  </div>
+                </td>
+                <tr><th>Alphonse</th> <td>alphonse@gmail.com</td>  <td>0600000000</td>  <td>Booker des TheWorld</td> <td>De confiance</td><td>
+                  <div class="btn-group" role="group" aria-label="...">
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-star-empty no-margin"></button>
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-envelope no-margin"></button>
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-user no-margin"></button>
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil no-margin"></button>
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-trash no-margin"></button>
+                  </div></td></tr>
+              </tobdy>
+            </table>
+          </div>
         </div>
-      </div>
-    </article>
-  </section>
+      </article>
+      <article class="col-lg-offset-1 col-lg-10 ">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <strong>Tout les contacts</strong>
+          </div>
+          <div class="panel-body">
+            <ul class="nav nav-tabs" role="tablist">
+              <li role="presentation" class="active"><a href="#all" aria-controls="all" role="tab" data-toggle="tab">Tous</a></li>
+              <li role="presentation"><a href="#booker" aria-controls="booker" role="tab" data-toggle="tab">Bookers</a></li>
+              <li role="presentation"><a href="#orga" aria-controls="orga" role="tab" data-toggle="tab">Organisateurs</a></li>
+            </ul>
+            <div class="tab-content">
+              <div role="tabpanel" id="all" class="tab-pane active">
+                <table class="table table-bordered">
+                  <colgroup>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col class="col-lg-2"/>
+                  </colgroup>
+                  <thead>
+                    <tr><th>Nom / Prenom</th><th>Mail</th><th>Telephone</th><th>Description</th><th>Notes</th><th>Actions</th></tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th>Jean Charle</th>
+                      <td>jean.c@gmail.com</td>
+                      <td>0600000000</td>
+                      <td>Organisateur du festival les rouge</td>
+                      <td>Serieux</td>
+                      <td>
+                        <div class="btn-group" role="group" aria-label="...">
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-star no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-envelope no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-user no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-trash no-margin"></button>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr><th>Alphonse</th> <td>alphonse@gmail.com</td>  <td>0600000000</td>  <td>Booker des TheWorld</td> <td>De confiance</td>
+                      <td>
+                        <div class="btn-group" role="group" aria-label="...">
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-star no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-envelope no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-user no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-trash no-margin"></button>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div role="tabpanel" id="booker" class="tab-pane">
+                <table class="table table-bordered">
+                  <colgroup>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col class="col-lg-2"/>
+                  </colgroup>
+                  <thead>
+                    <tr><th>Nom / Prenom</th><th>Mail</th><th>Telephone</th><th>Description</th><th>Notes</th><th>Actions</th></tr>
+                  </thead>
+                  <tbody>
+                    <tr><th>Alphonse</th> <td>alphonse@gmail.com</td>  <td>0600000000</td>  <td>Booker des TheWorld</td> <td>De confiance</td>
+                      <td>
+                        <div class="btn-group" role="group" aria-label="...">
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-star no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-envelope no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-user no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-trash no-margin"></button>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div role="tabpanel" id="orga" class="tab-pane">
+                <table class="table table-bordered">
+                  <colgroup>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col class="col-lg-2"/>
+                  </colgroup>
+                  <thead>
+                    <tr><th>Nom / Prenom</th><th>Mail</th><th>Telephone</th><th>Description</th><th>Notes</th><th>Actions</th></tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th>Jean Charle</th>
+                      <td>jean.c@gmail.com</td>
+                      <td>0699999999</td>
+                      <td>Organisateur du festival les rouge</td>
+                      <td>Serieux</td>
+                      <td>
+                        <div class="btn-group" role="group" aria-label="...">
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-star no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-envelope no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-user no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil no-margin"></button>
+                          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-trash no-margin"></button>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
     <?php include("../view/include/footer.view.php"); ?>
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="../data/js/bootstrap.min.js"></script>
   </body>
-  </html>
+</html>
--- code/view/messages.view.php
+++ code/view/messages.view.php
@@ -10,7 +10,7 @@
   <body>
     <?php include("../view/include/header.view.php"); ?>
     <section>
-      <div class="col-lg-offset-1 col-lg-3">
+      <div class="col-lg-offset-1 col-lg-4">
         <ul class="nav nav-tabs" role="tablist">
           <li role="presentation" class="active"><a href="#inbox" aria-controls="inbox" role="tab" data-toggle="tab">Receptions <span class="badge info">5</span></a></li>
           <li role="presentation"><a href="#sent" aria-controls="sent" role="tab" data-toggle="tab">Envois</a></li>
@@ -19,61 +19,53 @@
         <div class="tab-content">
           <div role="tabpanel" class="tab-pane active" id="inbox"><table id="list" class="table table-hover col-lg-12">
             <colgroup>
-              <col/>
-              <col/>
-              <col/>
+              <col class="col-lg-3"/>
+              <col class="col-lg-7"/>
+              <col class="col-lg-2"/>
             </colgroup>
             <tbody>
-              <tr class="info"><td>Jean</td><td>Oubli</td><td>13:15</td></tr>
-              <tr class="info"><td>Hervé</td><td>Concert</td><td>17 Nov</td></tr>
-              <tr class="info"><td>Marc</td><td>Camion</td><td>11 Nov</td></tr>
-              <tr class="info"><td>Henri</td><td>Festival du lac</td><td>7 Nov</td></tr>
-              <tr class="info"><td>Louise</td><td>Préparation scène</td><td>24 Sep</td></tr>
-              <tr class="info"><td>Jean</td><td>Bonjour</td><td>11 Sep</td></tr>
+              <tr class="info"><td>Jean</td><td><b>Oubli</b></td><td>13:15</td></tr>
+              <tr class="info"><td>Hervé</td><td><b>Concert</b></td><td>17 Nov</td></tr>
+              <tr class="info"><td>Marc</td><td><b>Camion</b></td><td>11 Nov</td></tr>
+              <tr class="info"><td>Henri</td><td><b>Festival du lac</b></td><td>7 Nov</td></tr>
+              <tr class="info"><td>Louise</td><td><b>Préparation scène</b></td><td>24 Sep</td></tr>
+              <tr class="info"><td>Jean</td><td><b>Bonjour</b></td><td>11 Sep</td></tr>
               <tr class="warning"><td>Laurianne</td><td>Annulation</td><td>5 Sep</td></tr>
               <tr><td>Marie</td><td>Achat</td><td>1 Sep</td></tr>
             </tobdy>
           </table></div>
           <div role="tabpanel" class="tab-pane" id="sent"><table class="table table-hover col-lg-12">
             <colgroup>
-              <col/>
-              <col/>
-              <col/>
+              <col class="col-lg-3"/>
+              <col class="col-lg-7"/>
+              <col class="col-lg-2"/>
             </colgroup>
             <tbody>
-              <tr><td>Jean</td><td>Oubli</td><td>13:15</td></tr>
-              <tr><td>Hervé</td><td>Concert</td><td>17 Nov</td></tr>
               <tr><td>Marc</td><td>Camion</td><td>11 Nov</td></tr>
               <tr><td>Henri</td><td>Festival du lac</td><td>7 Nov</td></tr>
               <tr><td>Louise</td><td>Préparation scène</td><td>24 Sep</td></tr>
-              <tr><td>Jean</td><td>Bonjour</td><td>11 Sep</td></tr>
-              <tr><td>Laurianne</td><td>Annulation</td><td>5 Sep</td></tr>
-              <tr><td>Marie</td><td>Achat</td><td>1 Sep</td></tr>
             </tobdy>
           </table></div>
           <div role="tabpanel" class="tab-pane" id="drafts"><table class="table table-hover col-lg-12">
             <colgroup>
-              <col/>
-              <col/>
-              <col/>
+              <col class="col-lg-3"/>
+              <col class="col-lg-7"/>
+              <col class="col-lg-2"/>
             </colgroup>
             <tbody>
-              <tr><td>Jean</td><td>Oubli</td><td>13:15</td></tr>
-              <tr><td>Hervé</td><td>Concert</td><td>17 Nov</td></tr>
-              <tr><td>Marc</td><td>Camion</td><td>11 Nov</td></tr>
+              <tr><td>Hervé</td><td><b>Concert</b></td><td>17 Nov</td></tr>
+              <tr><td>Marc</td><td><b>Camion</b></td><td>11 Nov</td></tr>
             </tobdy>
           </table></div>
         </div>
       </div>
-      <div class="col-lg-7">
+      <div class="col-lg-6">
         <div class="panel panel-default">
           <div class="panel-heading">
             <div class="row">
               <div class="col-lg-8">
-                <p>
-                  <strong>Message : Annulation</strong><br/>
-                  <small>de : Laurianne</small>
-                </p>
+                <h4>Message : Annulation</h4>
+                <small>de : Laurianne</small>
               </div>
               <div class="col-sm-4 text-right" style="padding-right:50px;">
                 <p>le <em>05/09/2015</em> à <em>18h30</em></p>
@@ -81,27 +73,38 @@
               <div class="row col-lg-12">
                 <div class="col-lg-8">
                   <ul class="list-inline">
-                    <li><a class="btn btn-default" href="#"><span class="glyphicon glyphicon-tag"></span> Festival du lac</a></li>
-                    <li><a class="btn btn-default" href="#"><span class="glyphicon glyphicon-tag"></span> Metz</a></li>
+                    <li><a class="btn btn-default" href="#"><span class="glyphicon glyphicon-tag no-margin"></span> Festival du lac</a></li>
+                    <li><a class="btn btn-default" href="#"><span class="glyphicon glyphicon-tag no-margin"></span> Metz</a></li>
                   </ul>
                 </div>
                 <div class="col-lg-4 text-right">
                   <div class="btn-group">
-                    <a class="btn btn-default" title="Tagger"><span class="glyphicon glyphicon-tags"></span></a>
-                    <a class="btn btn-default" title="Favori"><span class="glyphicon glyphicon-star-empty"></span></a>
-                    <a class="btn btn-default" title="Répondre"><span class="glyphicon glyphicon-comment"></span></a>
-                    <a class="btn btn-default" title="Transférer"><span class="glyphicon glyphicon-share-alt"></span></a>
+                    <a class="btn btn-default" title="Tagger"><span class="glyphicon glyphicon-tags no-margin"></span></a>
+                    <a class="btn btn-default" title="Favori"><span class="glyphicon glyphicon-star-empty no-margin"></span></a>
+                    <a class="btn btn-default" title="Répondre"><span class="glyphicon glyphicon-comment no-margin"></span></a>
+                    <a class="btn btn-default" title="Transférer"><span class="glyphicon glyphicon-share-alt no-margin"></span></a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
           <div class="panel-body">
-            <div>
+            <div class="well">
               <p>Bonjour,<br/>
               Je vous contacte car j'ai reçu de nouvelles informations et je ne pourrais pas venir ce Week-End. Je vous préviens donc que j'annule le rendez-vous.<br/>
               Cordialement,
               Laurianne</p>
+            </div>
+            <hr/>
+            <div>
+              <h4>Répondre</h4>
+              <div class="col-lg-10">
+                <textarea class="form-control">Rédigez votre réponse ici ...</textarea>
+              </div>
+              <div class="col-lg-2">
+                <button class="btn btn-primary btn-lg full">Envoyer</button>
+                <button class="btn btn-default full">Ouvrir l'éditeur</button>
+              </div>
             </div>
           </div>
         </div>
