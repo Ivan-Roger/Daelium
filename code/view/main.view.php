0837e80646be98602dcb38ca6f88d8eab23f4941
Ivan-Roger <ivan.bob.emile@gmail.com>
2015-11-21 12:17:22 +0100

    Design page d'accueil ok, centenu et texte a ecrire.

--- code/view/main.view.php
+++ code/view/main.view.php
@@ -20,36 +20,35 @@
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
           <div class="slide-full item active">
+            <!-- trouver image -->
+            <img src="https://cdn2.artstation.com/p/assets/images/images/001/023/866/large/samma-van-klaarbergen-patrick-watson2.jpg?1438415706" alt="Erreur chargement image" style="position: relative; bottom: 80%;" />
             <div class="carousel-caption">
               <h1>Bienvenue ! A FAIRE !!!</h1>
               <p>Bienvenue sur Dælium, le site est actuellement en construction ...</p>
-              <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
             </div>
           </div>
           <div class="slide-full item">
+            <img src="https://s1.qwant.com/thumbr/?u=http%3A%2F%2Fwww.listofimages.com%2Fwp-content%2Fuploads%2F2012%2F05%2Fscene-light-hands-concertmusic.jpg" alt="Erreur chargement image" style="position: relative; bottom: 70%;"/>
             <div class="carousel-caption">
               <h1>Slide 2</h1>
               <p>Bienvenue sur Dælium, le site est actuellement en construction ...</p>
-              <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
             </div>
           </div>
         </div>
-
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-welcome" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-welcome" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
       </div>
     </section>
+    <div class="sliderPlace"></div>
     <section class="container-fluid full page-content">
       <article class="col-sm-8 col-sm-offset-1">
         <!-- Alertes et information pour l'utilisateur -->
-        <div>
+        <div class="well">
+          <img src="http://www.ac-grenoble.fr/ien.vienne1-2/spip/IMG/bmp_Image004.bmp" alt="Mountain View" style="height:100px;">
+          <p>Charle DURAND à rendu libre les inscription pour son evenement "Festival des eaux de la pluie". Vous pouvez maintenent rentre en contact avec lui afin d'y Inscrire un Artiste/Groupe .</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        </div>
+        <div class="well">
           <img src="http://www.ac-grenoble.fr/ien.vienne1-2/spip/IMG/bmp_Image004.bmp" alt="Mountain View" style="height:100px;">
           <p>Charle DURAND à rendu libre les inscription pour son evenement "Festival des eaux de la pluie". Vous pouvez maintenent rentre en contact avec lui afin d'y Inscrire un Artiste/Groupe .</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
@@ -59,7 +58,7 @@
       </article>
       <aside class="col-sm-2">
         <!-- Sugestion pour l'utilisateur (evt & art pour booker) (art pour orga )-->
-        <div>
+        <div class="well">
           <img src="http://www.ac-grenoble.fr/ien.vienne1-2/spip/IMG/bmp_Image004.bmp" alt="Mountain View" style="height:100px;">
           <p>L'evenement Bringue a la mairie de Valence est suceptible d'interesser le Groupe/Artiste "Les Bleu" que vous gerez.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
