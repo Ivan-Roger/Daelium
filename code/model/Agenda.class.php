ece366ae75aedb59a6f10f3a3997d61868a6d275
Katravan <florian.inard@live.fr>
2015-12-04 17:48:53 +0100

    Constructeur + getters

--- code/model/Agenda.class.php
+++ code/model/Agenda.class.php
@@ -3,8 +3,28 @@
     private User $user;
     private AgendaEvent[] $events;
 
-    function __construct() {
-
+    // Constructeur
+    function __construct($user=NULL, $events=NULL) { // A vroi si bonne écriture pour $events
+      if (!isset($this->user)) {
+        $this->user = $user;
+      }
+      if ($events != NULL) {
+        $this->events = $events;
+      }
     }
+    
+  // ********************************
+  //        Fonctions getter
+  // ********************************
+  // getter de l'utilisateur à qui appartient l'agenda
+  function getUser() {
+    return $this->user;
+  }
+    
+  // getter de la liste des évènements 
+  function getEvents() {
+    return $this->events;
+  }
+    
   }
 ?>
