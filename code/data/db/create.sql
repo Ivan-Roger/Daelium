0eeb57815130d27076f149cffa6e275daf6e7ae8
EtienneBarbier <etienne.jeux@gmail.com>
2015-12-06 22:52:58 +0100

    Base de données
    La base de donner a ete cree sur le vps

---
+++ code/data/db/create.sql
@@ -0,0 +1,249 @@
+CREATE TABLE lieu (
+  idLieu        SERIAL PRIMARY KEY,
+  nom           VARCHAR(255) NOT NULL,
+  description   VARCHAR(1000),
+  pays          VARCHAR(255),
+  region        VARCHAR(255),
+  ville         VARCHAR(255),
+  codePostal    numeric(5),
+  adresse       VARCHAR(255),
+  latitude      decimal,
+  longitude     decimal
+);
+CREATE TABLE Personne (
+  idPersonne     SERIAL PRIMARY KEY,
+  nom            VARCHAR (255) NOT NULL,
+  prenom         VARCHAR (255),
+  emailcontact   VARCHAR (255) NOT NULL,
+  tel            numeric(10),
+  adresse        BIGINT,
+  FOREIGN KEY (adresse) references Lieu(idLieu)
+);
+
+CREATE TABLE Utilisateur (
+  idUtilisateur SERIAL PRIMARY KEY,
+  emailCompte VARCHAR(255) NOT NULL,
+  mdp VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Artiste (
+  idArtiste     BIGINT PRIMARY KEY,
+  dateNaissance date,
+  paiement      integer,
+  rib           VARCHAR(255),
+  ordreCheque   VARCHAR(255),
+  FOREIGN KEY (idArtiste) references Personne(idPersonne)
+);
+
+CREATE TABLE Booker (
+  idBooker BIGINT PRIMARY KEY,
+  FOREIGN KEY (idBooker) references Utilisateur(idUtilisateur)
+);
+
+CREATE TABLE Organisateur (
+  idOrganisateur BIGINT PRIMARY KEY,
+  FOREIGN KEY (idOrganisateur) references Utilisateur(idUtilisateur)
+);
+
+CREATE TABLE Contact (
+  Proprietaire  BIGINT,
+  idContact     integer,
+  notes         VARCHAR(255),
+  PRIMARY KEY (Proprietaire,idContact),
+  FOREIGN KEY (Proprietaire) references Utilisateur(idUtilisateur)
+);
+
+
+CREATE TABLE Contact_System (
+  contactProprietaire BIGINT,
+  idContact           integer,
+  idPersonne          BIGINT,
+  PRIMARY KEY (ContactProprietaire,idContact),
+  FOREIGN KEY (ContactProprietaire,idContact) references Contact(Proprietaire,idContact),
+  FOREIGN KEY (idPersonne) references Personne(idPersonne)
+);
+
+CREATE TABLE Contact_exterieur (
+  contactProprietaire BIGINT,
+  idContact           integer,
+  nom                 VARCHAR(255) NOT NULL,
+  mail                VARCHAR(255) UNIQUE,
+  tel                 numeric(10),
+  PRIMARY KEY (ContactProprietaire,idContact),
+  FOREIGN KEY (ContactProprietaire,idContact) references Contact(Proprietaire,idContact)
+);
+
+CREATE TABLE Manifestation (
+  idManif           SERIAL PRIMARY KEY,
+  type              VARCHAR(255),
+  description       VARCHAR(255),
+  dateDebut         DATE NOT NULL,
+  dateFin           DATE NOT NULL,
+  prixPublic        integer,
+  lienImageOfficiel VARCHAR(255),
+  facebook          VARCHAR(255),
+  google            VARCHAR(255),
+  twitter           VARCHAR(255),
+  fichecom          VARCHAR(255),
+  createur          BIGINT,
+  lieu              BIGINT,
+  FOREIGN KEY (createur) references Organisateur(idOrganisateur),
+  FOREIGN KEY (lieu) references Lieu(idLieu)
+);
+
+CREATE TABLE Groupe (
+  idGroupe          SERIAL PRIMARY KEY,
+  nom               VARCHAR(255) NOT NULL,
+  nbMembre          integer NOT NULL,
+  email             VARCHAR(255),
+  lienImageOfficiel VARCHAR(255),
+  facebook          VARCHAR(255),
+  google            VARCHAR(255),
+  twitter           VARCHAR(255),
+  soundcloud        VARCHAR(255),
+  lecteur           VARCHAR(255),
+  fichecom          VARCHAR(255),
+  adresse           BIGINT,
+  FOREIGN KEY (adresse) references Lieu(idLieu)
+);
+
+CREATE TABLE Genre (
+  nomg  VARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE Document (
+  idDoc           SERIAL PRIMARY KEY,
+  idUtilisateur   BIGINT,
+  nom             VARCHAR(255) NOT NULL,
+  dateCreation    DATE,
+  datemodif       DATE,
+  emplacement     integer,
+  FOREIGN KEY (idUtilisateur) references Utilisateur(idUtilisateur)
+);
+
+CREATE TABLE Evenement (
+  idEvene       SERIAL PRIMARY KEY,
+  datedebut     DATE NOT NULL,
+  datefin       DATE NOT NULL,
+  heuredebut    TIME(6) NOT NULL,
+  heurefin      TIME(6) NOT NULL,
+  description   VARCHAR(255),
+  lieu          integer references Lieu(idLieu),
+  createur      integer references Utilisateur(idUtilisateur)
+);
+
+CREATE TABLE Contact_Evenement (
+  idEvene BIGINT references Evenement(idEvene),
+  Proprietaire  BIGINT,
+  idContact     integer,
+  PRIMARY KEY(idEvene,Proprietaire,idContact),
+  FOREIGN KEY (idContact,Proprietaire) references Contact(idContact,Proprietaire)
+);
+
+CREATE TABLE Tag (
+  nomt VARCHAR(255) PRIMARY KEY
+);
+
+
+CREATE TABLE Creneau (
+  idManif         BIGINT,
+  idGroupe        BIGINT,
+  heureDebut      TIME(6),
+  heureFin        TIME(6),
+  heureDebutTest  TIME(6),
+  heureFinTest    TIME(6),
+  PRIMARY KEY (idManif,idGroupe,heureDebut),
+  FOREIGN KEY (idManif) references Manifestation(idManif),
+  FOREIGN KEY (idGroupe) references Groupe(idGroupe)
+);
+
+CREATE TABLE Groupe_Artiste (
+  idGroupe  BIGINT,
+  idArtiste BIGINT,
+  PRIMARY KEY (idGroupe,idArtiste),
+  FOREIGN KEY (idGroupe) references Groupe(idGroupe),
+  FOREIGN KEY (idArtiste) references Artiste(idArtiste)
+);
+
+CREATE TABLE Negociation ( -- Le Quaduor est unique
+  idNegociation SERIAL PRIMARY KEY,
+  idBooker BIGINT,
+  idManif BIGINT,
+  idGroupe BIGINT,
+  idOrganisateur BIGINT,
+  etat integer,
+  FOREIGN KEY (idBooker) references Booker(idBooker),
+  FOREIGN KEY (idManif) references Manifestation(idManif),
+  FOREIGN KEY (idGroupe) references Groupe(idGroupe),
+  FOREIGN KEY (idOrganisateur) references Organisateur(idOrganisateur)
+);
+
+CREATE TABLE Negociation_Documents (
+  idNegociation BIGINT,
+  idDoc BIGINT,
+  PRIMARY KEY (idNegociation,idDoc),
+  FOREIGN KEY (idNegociation) references Negociation(idNegociation),
+  FOREIGN KEY (idDoc) references Document(idDoc)
+);
+
+CREATE TABLE Message (
+  idMessage SERIAL PRIMARY KEY,
+  expediteur BIGINT,
+  receveur  BIGINT,
+  etat integer,
+  contenu varchar(255),
+  dateenvoi DATE,
+  FOREIGN KEY (receveur) references Utilisateur(idUtilisateur),
+  FOREIGN KEY (expediteur) references Utilisateur(idUtilisateur)
+);
+
+CREATE TABLE Message_Tag (
+  nomt VARCHAR(255) NOT NULL,
+  idMessage BIGINT,
+  PRIMARY KEY (nomt,idMessage),
+  FOREIGN KEY (nomt) references Tag(nomt),
+  FOREIGN KEY (idMessage) references Message(idMessage)
+);
+
+CREATE TABLE Contact_Tag (
+  nomt VARCHAR(255) NOT NULL,
+  Proprietaire  BIGINT,
+  idContact     integer,
+  PRIMARY KEY (nomt,Proprietaire,idContact),
+  FOREIGN KEY (nomt) references Tag(nomt),
+  FOREIGN KEY (idContact,Proprietaire) references Contact(idContact,Proprietaire)
+);
+
+CREATE TABLE Negociation_Messages (
+  idNegociation BIGINT,
+  idMessage BIGINT,
+  PRIMARY KEY (idNegociation,idMessage),
+  FOREIGN KEY (idNegociation) references Negociation(idNegociation),
+  FOREIGN KEY (idMessage) references Message(idMessage)
+);
+
+
+CREATE TABLE Booker_Groupe (
+  idBooker BIGINT,
+  idGroupe BIGINT,
+  PRIMARY KEY (idBooker,idGroupe),
+  FOREIGN KEY (idBooker) references Booker(idBooker),
+  FOREIGN KEY (idGroupe) references Groupe(idGroupe)
+);
+
+
+CREATE TABLE Groupe_Genre (
+  idGroupe BIGINT,
+  nomg  VARCHAR(255),
+  PRIMARY KEY (nomg,idGroupe),
+  FOREIGN KEY (idGroupe) references Groupe(idGroupe),
+  FOREIGN KEY (nomg) references Genre(nomg)
+);
+
+CREATE TABLE Manifestation_Genre (
+  idManif BIGINT,
+  nomg  VARCHAR(255),
+  PRIMARY KEY (nomg,idManif),
+  FOREIGN KEY (idManif) references Manifestation(idManif),
+  FOREIGN KEY (nomg) references Genre(nomg)
+);
