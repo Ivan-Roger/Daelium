76a2dc8113a5bae20ab5d3905435c23655926025
Ivan Roger <ivan.roger@iut2.upmf-grenoble.fr>
2015-12-18 15:57:17 +0100

    Debut co/deco via google

--- code/view/evenements.view.php
+++ code/view/evenements.view.php
@@ -31,5 +31,7 @@
     <?php include("../view/include/footer.view.php"); ?>
     <script src="../data/js/jQuery.min.js"></script>
     <script src="../data/js/bootstrap.min.js"></script>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <script src="../data/js/common.js"></script>
   </body>
 </html>
