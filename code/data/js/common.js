1c8e53bece7511ec20ceea352b6b7de2ab4bdcef
Ivan Roger <ivan.roger@iut2.upmf-grenoble.fr>
2015-12-18 18:09:22 +0100

    Fix - Working (j'ai testé :D)

--- code/data/js/common.js
+++ code/data/js/common.js
@@ -1,4 +1,6 @@
 function onLoad() {
+  console.log("Initialized !");
+  $("#logoutBtn").on('click',signOut);
   gapi.load('auth2', function() {
     gapi.auth2.init();
   });
@@ -8,6 +10,6 @@ function signOut() {
   auth2.signOut().then(function () {
     console.log('User signed out.');
   });
-  document.location="../controler/connexion.ctrl.php?logout";
+  window.location="../controler/connexion.ctrl.php?logout";
 }
-$("#logoutBtn").click(signOut);
+window.onload=onLoad;
