fc06993deb1042ba682e7d6afc7391c4267aca5a
Katravan <florian.inard@live.fr>
2015-12-23 13:29:09 +0100

    Modifications (pas du fonctionnel)

--- code/model/Artiste.class.php
+++ code/model/Artiste.class.php
@@ -24,29 +24,43 @@ require_once('../model/Personne.class.php');
       }
     }
 
-    //GETTERS
+    // ********************************
+    //        Fonctions getter
+    // ********************************
+    // getter de la date de naissance de l'artiste
     function getDateNaissance() {
       return $this->dateNaissance;
     }
+    // getter du paiement de l'artiste ??????
     function getPaiement() {
       return $this->paiement;
     }
+    // getter du RIB de l'artiste
     function getRib() {
       return $this->rib;
     }
+    // getter de l'ordre auquel adresser les chèques pour l'artiste
     function getOrdreCheque() {
       return $this->ordreCheque;
     }
-    //SETTERS
+
+    
+    // ********************************
+    //        Fonctions getter
+    // ********************************
+    // setter de la date de naissance de l'artiste
     function setDateNaissance($dateNaissance) {
      $this->dateNaissance = $dateNaissance;
     }
+    // setter du paiement de l'artiste ??????
     function setPaiement($paiement) {
      $this->paiement = $paiement;
     }
+    // setter du RIB de l'artiste
     function setRib($rib) {
      $this->rib = $rib;
     }
+    // setter de l'ordre auquel adresser les chèques pour l'artiste
     function setOrdreCheque($ordreCheque) {
      $this->ordreCheque = $ordreCheque;
     }
