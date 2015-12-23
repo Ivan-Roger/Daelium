6d0e7ac6f9e1df50c74f25389499e5e4fd946dfe
EtienneBarbier <etienne.jeux@gmail.com>
2015-12-23 18:41:27 +0100

    Update

--- code/controler/evenement_fiche.ctrl.php
+++ code/controler/evenement_fiche.ctrl.php
@@ -1,58 +1,129 @@
 <?php
-  session_start();
-  include("include/auth.ctrl.php");
-  require_once("../model/utils.class.php");
-  $data = initPage("Groupes");
+session_start();
+//include_once("../model/Artist.class.php");
+include("include/auth.ctrl.php");
+require_once("../model/utils.class.php");
+require_once("../model/DAO.class.php");
+$data = initPage("Events");
+$dao = new Dao();
+
+$userid= $_SESSION["user"]["ID"];
+$user = $dao->readPersonneById($userid);
+if($user->getType() == 1){ // SI ce n'est pas un organisateur alors a un message d'erreur .
+  if (isset($_GET['id']) && $_GET['id'] != "") { // Si il n'y a pas d'id alors -> message d'erreur
+
+    // Recupérer les données depuis la BD avec l'id ($_GET['id'])
+    $evtid = $_GET['id'];
+    $evt = $dao->readManifestationById($evtid);
+
+    //On verifie que la manifestation appartient bien a l'organisateur.
+    $listeevtuser = $dao->readIdManifestationByCreateur($userid);
+    $present = false;
+    foreach ($listeevtuser as $key => $value) {
+      if($value['idmanif'] == $evtid && !$present){
+        $present = true;
+      }
+    }
+    if($present){ //Si il est le proprietaire de l'evt alors on affiche. sinon message d'erreur
+      //Information sur l'evt !
+      $data['evenement']['id'] = $_GET['id'];
+      $data['evenement']['nom'] = $evt->getNom();
+      if(($idlieu = $evt->getLieu()) == NULL){
+      $data['evenement']['lieu']['adresse'] = "Non indiquer";
+      $data['evenement']['lieu']['googlemaps'] = NULL;
+      }else{
+
+       $lieu = $dao->readLieuById($idlieu);
+       $data['evenement']['lieu']['adresse'] =  $lieu->getAdresse().", ".$lieu->getcodepostal().", ".$lieu->getVille().", ".$lieu->getPays();
+       $data['evenement']['lieu']['googlemaps'] = "https://www.google.fr/maps/place/".str_replace ( ' ' ,'+' ,$lieu->getAdresse())."+".$lieu->getcodepostal()."+".$lieu->getVille()."+".strtolower($lieu->getPays());
+      }
 
 
-  $data["action"] = "";
-  if (isset($_GET["id"]) && isset($_GET["action"]) && $_GET['action']=="edit") {
-    $data['groupe']['id']= $_GET['id'];
-    $data['groupe']['nom']= "En Marche";
-    include("../view/groupe_fiche_edit.view.php");
-  } else if(isset($_GET["id"])) {
+      // Info sur les passages
+      $pas['date'] = "18/11/2015 21h20";
+      $pas['groupe']['nom'] = "En marche";
+      $data['passages'][] = $pas;
 
-    $data['groupe']['id']=$_GET['id'];
-    $data['groupe']['nom'] ="En Marche";
-    $data['groupe']['nb']=2;
+      $pas['date'] = "18/11/2015 21h50";
+      $pas['groupe']['nom'] = "Batoucada";
+      $data['passages'][] = $pas;
 
-    $art['prenom'] = "Jean-Louis";
-    $art['nom'] = "Dupond";
-    $art['role'] = "Guitariste Chanteur";
-    $art['desc'] = "Guitariste du groupe et chanteur";
-    $data['artistes'][] = $art;
+      $data['evenement']['img'] = "../data/users/icons/bilbao-logo.jpg";
+      include("../view/evenement_fiche.view.php");
+    }else {
+      $data['error']['title'] = "Acces Interdit";
+      $data['error']['message'] = "Vous ne pouvez pas venir ici, cet espace est reservé à l'organisateur de la manifestation.";
+      $data['error']['back'] = "../controler/main.ctrl.php";
+      include("../view/error.view.php");
+    }
 
-    $art['prenom'] = "Laurent";
-    $art['nom'] = "Dupuis";
-    $art['role'] = "Batteur";
-    $art['desc'] = "Batteur du groupe";
-    $data['artistes'][] = $art;
+  } elseif (isset($_GET['action']) && $_GET['action']=="new") {
+    //si il demande la creation d'un evt
+    $data['evenement']['id'] = "kfpb5gso63w7s2l";
+    $data['evenement']['nom'] = "Nouvel Evenement";
+    //envoi un formulaire pour cree un artiste
+    include("../view/evenement_edit.view.php");
 
-    $album['nom'] = "Hon Hop";
-    $album['date'] = "2013";
-    $data['albums'][] = $album;
+  } elseif (isset($_POST['action']) && $_POST['action']="dates") {
+    //quand il a rempli le 1er formulaire
+    $data['evenement']['id'] = $_POST['id'];
+    $data['evenement']['nom'] = "Bilbao BBK Live";
+    $data['evenement']['img'] = "../data/users/icons/bilbao-logo.jpg";
+    $dated = $_POST['dated'];
+    $datef = $_POST['datef'];
 
-    $album['nom'] = "Ping Pong";
-    $album['date'] = "2015";
-    $data['albums'][] = $album;
+    function getDatesBetween ($dStart, $dEnd) {
+      $iStart = strtotime ($dStart);
+      $iEnd = strtotime ($dEnd);
+      if (false === $iStart || false === $iEnd) {
+        return false;
+      }
+      $aStart = explode ('-', $dStart);
+      $aEnd = explode ('-', $dEnd);
+      if (count ($aStart) !== 3 || count ($aEnd) !== 3) {
+        return false;
+      }
+      if (false === checkdate ($aStart[1], $aStart[2], $aStart[0]) || false === checkdate ($aEnd[1], $aEnd[2], $aEnd[0]) || $iEnd <= $iStart) {
+        return false;
+      }
+      for ($i = $iStart; $i < $iEnd + 86400; $i = strtotime ('+1 day', $i) ) {
+        $sDateToArr = strftime ('%d/%m/%Y', $i);
+        $sYear = substr ($sDateToArr, 0, 4);
+        $sMonth = substr ($sDateToArr, 5, 2);
+        $aDates[] = $sDateToArr;
+      }
+      if (isset ($aDates) && !empty ($aDates)) {
+        return $aDates;
+      } else {
+        return false;
+      }
+    }
 
-    $lineUp['nom'] = "Hello";
-    $lineUp['url'] = "https://www.youtube.com/embed/YQHsXMglC9A?feature=player_detailpage";
-    $data['lineUp'][] = $lineUp;
+    // chaque jour qu'il y a entre ces deux dates
+    $aDates = getDatesBetween ($dated, $datef);
+    //$data['evenement']['dates'] = $aDates;
 
-    $lineUp['nom'] = "Overwerk";
-    $lineUp['url'] = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/231318729&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true";
-    $data['lineUp'][] = $lineUp;
+    $data['evenement']['dates'][] = "15/11/2014";
+    $data['evenement']['dates'][] = "16/11/2014";
+    $data['evenement']['dates'][] = "17/11/2014";
+    $data['evenement']['dates'][] = "18/11/2014";
 
-    //envoie les données pour un artiste
-    include("../view/groupe_fiche.view.php");
+    //insere dans le Dao
+
+    include("../view/evenement_dates.view.php");
+
   } else {
-    $data['error']['title'] = "Groupe Inconnu";
-    $data['error']['back'] = "../controler/main.ctrl.php";
-    $data['error']['message'] = "Le groupe que vous cherchez n'existe pas ou plus !";
+    $data['error']['title'] = "Evenement inconnu";
+    $data['error']['back'] = "../controler/evenements.ctrl.php";
+    $data['error']['message'] = "Vous vous etes perdu ...";
+    $data['page'] = "Error";
     include("../view/error.view.php");
   }
 
-
-
+}else {
+  $data['error']['title'] = "Acces Interdit";
+  $data['error']['message'] = "Vous ne pouvez pas venir ici, cet espace est reservé à l'organisateur de la manifestation.";
+  $data['error']['back'] = "../controler/main.ctrl.php";
+  include("../view/error.view.php");
+}
 ?>
