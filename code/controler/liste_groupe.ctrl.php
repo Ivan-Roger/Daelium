8628536706f53b8a6b83c30df82e0bf0a0618cd0
EtienneBarbier <etienne.jeux@gmail.com>
2015-12-30 17:33:51 +0100

    Base Listes

---
+++ code/controler/liste_groupe.ctrl.php
@@ -0,0 +1,14 @@
+<?php
+  session_start();
+  include("include/auth.ctrl.php");
+  require_once("../model/utils.class.php");
+  require_once("../model/DAO.class.php");
+  $data = initPage("Liste Groupes");
+
+  $dao = new DAO();
+
+
+
+
+  include("../view/liste_groupe.view.php");
+?>
